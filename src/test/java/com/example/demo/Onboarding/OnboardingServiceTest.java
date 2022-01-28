77e47f3c7077521817ded56384f65c031525920a
akash_garg <akashgarg3a@gmail.com>
2022-01-29 02:39:10 +0530

    Added 100% test coverage for OnboardingSerive class

--- src/test/java/com/example/demo/Onboarding/OnboardingServiceTest.java
+++ src/test/java/com/example/demo/Onboarding/OnboardingServiceTest.java
@@ -1,21 +1,24 @@
 package com.example.demo.Onboarding;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class OnboardingServiceTest {
@@ -24,6 +27,8 @@ class OnboardingServiceTest {
     private OnboardingRepository onboardingRepository;
     private OnboardingService underTest;
 
+    @Mock
+    private Employee employee;
 
     @BeforeEach
     void setUp() {
@@ -57,6 +62,8 @@ class OnboardingServiceTest {
         ArgumentCaptor<Employee> employeeArgumentCaptor = ArgumentCaptor.forClass(Employee.class);
 
         verify(onboardingRepository).save(employeeArgumentCaptor.capture());
+        verify(onboardingRepository, times(1)).findEmailExist(emp.getEmail());
+        verify(onboardingRepository, times(1)).save(any(Employee.class));
 
         Employee captureEmployee = employeeArgumentCaptor.getValue();
 
@@ -75,18 +82,103 @@ class OnboardingServiceTest {
         );
 
         // when
-        given(onboardingRepository.findEmailExist(emp.getEmail())).willReturn(true);
+        given(onboardingRepository.findEmailExist(anyString()) ).willReturn(true);
 
         // then
         assertThatThrownBy(() -> underTest.addNewEmployee(emp) ).isInstanceOf(IllegalStateException.class).hasMessageContaining("Email already Exit");
-
+        verify(onboardingRepository, never()).save(any());
     }
 
     @Test
-    void deleteEmployee() {
+    void checkDeleteEmployee() {
+        // given
+        Employee emp = new Employee(
+                "Akash",
+                "Garg",
+                "",
+                "akash.garg@gmail.com",
+                LocalDate.of(1996, 11, 15)
+        );
+        long id = 1;
+
+        // when
+        Mockito.when(onboardingRepository.findById(id)).thenReturn(Optional.of(emp));
+
+        // then
+        underTest.deleteEmployee(id);
+        verify(onboardingRepository, times(1)).findById(id);
+        verify(onboardingRepository, times(1)).deleteById(id);
+    }
+
+    @Test
+    void checkDeleteEmployeeThrowException() {
+        //given
+        long id = 1;
+
+        //when
+        //then
+        assertThatThrownBy(()-> underTest.deleteEmployee(id)).isInstanceOf(IllegalStateException.class).hasMessageContaining("No Employee with Id Found");
+        verify(onboardingRepository, never()).deleteById(id);
     }
 
     @Test
     void updateEmployee() {
+        // given
+        Employee emp = new Employee(
+                "Akash",
+                "Garg",
+                "",
+                "akash.garg@gmail.com",
+                LocalDate.of(1996, 11, 15)
+        );
+        long id = 1;
+        String firstName = "ankush";
+        String email = "ankush.garg@gmail.com";
+        emp.setId(id);
+
+        // when
+        Mockito.when(onboardingRepository.findById(id)).thenReturn(Optional.of(emp));
+        underTest.updateEmployee(id, firstName, email);
+
+        // then
+        assert(emp.getEmail() == email);
+        assert(emp.getFirstName() == firstName);
+
+    }
+
+    @Test
+    void checkUpdateEmployeeThrowExceptionForId() {
+        //given
+        long id = 1;
+        String firstName = "ankush";
+        String email = "ankush.garg@gmail.com";
+
+        //when
+        //then
+        assertThatThrownBy(()-> underTest.updateEmployee(id, firstName, email)).isInstanceOf(IllegalStateException.class).hasMessageContaining("id not found");
+//        verify(onboardingRepository, never()).deleteById(1l);
+    }
+
+    @Test
+    void checkUpdateEmployeeThrowExceptionForEmail() {
+        // given
+        Employee emp = new Employee(
+                "Akash",
+                "Garg",
+                "",
+                "akash.garg@gmail.com",
+                LocalDate.of(1996, 11, 15)
+        );
+        long id =1;
+        emp.setId(id);
+        String firstName = "ankush";
+        String email = "ankush.garg@gmail.com";
+
+        // when
+        Mockito.when(onboardingRepository.findById(id)).thenReturn(Optional.of(emp));
+        Mockito.when(onboardingRepository.findEmailExist(email)).thenReturn(true);
+
+        // then
+        assertThatThrownBy(() -> underTest.updateEmployee(1l,firstName, email) ).isInstanceOf(IllegalStateException.class).hasMessageContaining("email already taken");
     }
 }
